1cc86cf8d06d785300f6c0eeb7e6f58877d2585d
Michael Kuang <kgz406@gmail.com>
2016-09-15 15:46:49 -0400

    Create lazyload.js

---
+++ js/lazyload.js
@@ -0,0 +1,45 @@
+window.onload = function loadStuff() {
+  var win, doc, img, header, enhancedClass;
+
+  if (!('addEventListener' in window)) {
+    return;
+  }
+
+  win = window;
+  doc = win.document;
+  img = new Image();
+  header = doc.querySelector('.content');
+  enhancedClass = 'content-enhanced';
+
+  var bigSrc = (function() {
+
+    var styles = doc.querySelector('style').sheet.cssRules;
+
+    var bgDecl = (function() {
+
+      var bgStyle, i, l = styles.length;
+      for (i = 0; i < l; i++) {
+
+        if (styles[i].selectorText &&
+          styles[i].selectorText == '.' + enhancedClass) {
+          
+          bgStyle = styles[i].style.backgroundImage;
+
+          break;
+        }
+      }
+
+      return bgStyle;
+    }());
+      
+    return bgDecl && bgDecl.match(/(?:\(['|"]?)(.*?)(?:['|"]?\))/)[1];
+  }());
+
+  img.onload = function() {
+    header.className += ' ' + enhancedClass;
+  };
+
+  if (bigSrc) {
+    img.src = bigSrc;
+  }
+};
